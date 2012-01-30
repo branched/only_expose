171eec9e3e97b6337f96928c8e5e62815b198de6
Chris Harper <charper@branched.co.uk>
2012-01-30 18:38:23 +0000

    Use ActiveSupport::Concern over old method

--- lib/only_expose.rb
+++ lib/only_expose.rb
@@ -1,25 +1,21 @@
+require 'active_support/concern' 
 module OnlyExpose
+  extend ActiveSupport::Concern
 
-  def self.included(base)
-    base.extend ClassMethods
-  end
+  included do  
+    def to_json(options = {})
+      super(options.merge(:only => self.class.exposed_attributes))
+    end
 
-  #Instance Methods
-  
-  def to_json(options = {})
-    super(options.merge(:only => self.class.exposed_attributes))
-  end
-
-  def to_xml(options = {})
-    super(options.merge(:only => self.class.exposed_attributes))
+    def to_xml(options = {})
+      super(options.merge(:only => self.class.exposed_attributes))
+    end
   end
 
   module ClassMethods
-    
     def only_expose(*args)
       @exposed_attributes = *args
     end
-
     def exposed_attributes
       if @exposed_attributes.nil? && self.superclass != ActiveRecord::Base
          superclass.exposed_attributes
